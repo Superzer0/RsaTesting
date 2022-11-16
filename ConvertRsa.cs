1e2104d345ba9d08b60f23f3b7dd0db48bbf058c
Jakub Kawa <jka@leapwork.com>
2022-11-16 08:29:08 +0100

    Add project files.

---
+++ ConvertRsa.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal static class ConvertRsa
+    {
+        // PKCS#1 format
+        public static string FromPemToPkcs1Pem(string publicKey)
+        {
+            var rsaPublicKey = RSA.Create();
+            rsaPublicKey.ImportFromPem(publicKey);
+
+            var buffer = new StringBuilder();
+            buffer.AppendLine("-----BEGIN RSA PUBLIC KEY-----");
+            buffer.AppendLine(Convert.ToBase64String(
+                rsaPublicKey.ExportRSAPublicKey(),
+                Base64FormattingOptions.InsertLineBreaks));
+            buffer.AppendLine("-----END RSA PUBLIC KEY-----");
+
+            return buffer.ToString();
+        }
+
+        public static string FromPublicPemToPkcs8Pem(string publicKey)
+        {
+            var rsaPublicKey = RSA.Create();
+            rsaPublicKey.ImportFromPem(publicKey);
+
+            var buffer = new StringBuilder();
+            buffer.AppendLine("-----BEGIN PUBLIC KEY-----");
+            buffer.AppendLine(System.Convert.ToBase64String(
+                rsaPublicKey.ExportSubjectPublicKeyInfo(),
+                Base64FormattingOptions.InsertLineBreaks));
+            buffer.AppendLine("-----END PUBLIC KEY-----");
+
+            return buffer.ToString();
+        }
+
+        // PKCS#8 format
+        public static string FromPublicPkcs1ToPkcs8Pem(string publicKey)
+        {
+            var rsaPublicKey = RSA.Create();
+            rsaPublicKey.ImportRSAPublicKey(System.Convert.FromBase64String(publicKey), out _);
+
+            var buffer = new StringBuilder();
+            buffer.AppendLine("-----BEGIN PUBLIC KEY-----");
+            buffer.AppendLine(System.Convert.ToBase64String(
+                rsaPublicKey.ExportSubjectPublicKeyInfo(),
+                Base64FormattingOptions.InsertLineBreaks));
+            buffer.AppendLine("-----END PUBLIC KEY-----");
+
+            return buffer.ToString();
+        }
+
+        public static void WriteToConsole(string input, Func<string, string> convert)
+        {
+            var output = convert(input);
+            Console.WriteLine("Input:");
+            Console.WriteLine(input);
+            Console.WriteLine("Output:");
+            Console.WriteLine(output);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+
+Console.WriteLine("");
+
+var rsaPublicKeyPkcs1 =
+    "MIIBCgKCAQEAx2fQatW9pxi0rM3nJYZ1BQB5WOVppsiR8ivcT9MChxM4V1KijYMn2xAZ/6UhRu/m2HKkJjVAVofmzbLlGvIVjd2lNAyMTV7srR1eQ+kwM+kXBeW5exAoB7k/KttKMkk4Gi7rMSxOxHbV44SQS+10LzgEhJ6sAbihrXzbplTLrW+bkp1Mkl1UB+NnHCwe3af+PZvUOQrrI4MyrBsUtvH3OBVaCy16D4BjWBeUF3MZa002vPRZR2JpJpSCmkNTsykYFFJKqE3m+rQgLtDzwP+qvVqS7Ds5mNypgWEuTzdPSyZJGlsC0JFxRwjhtNbcpA49dQ8H50cRCH1butN22oVTLQIDAQAB";
+
+var rsaPublicKeyPkcs1WithLineBreaks = @"MIIBCgKCAQEAx2fQatW9pxi0rM3nJYZ1BQB5WOVppsiR8ivcT9MChxM4V1KijYMn2xAZ/6UhRu/m
+2HKkJjVAVofmzbLlGvIVjd2lNAyMTV7srR1eQ+kwM+kXBeW5exAoB7k/KttKMkk4Gi7rMSxOxHbV
+44SQS+10LzgEhJ6sAbihrXzbplTLrW+bkp1Mkl1UB+NnHCwe3af+PZvUOQrrI4MyrBsUtvH3OBVa
+Cy16D4BjWBeUF3MZa002vPRZR2JpJpSCmkNTsykYFFJKqE3m+rQgLtDzwP+qvVqS7Ds5mNypgWEu
+TzdPSyZJGlsC0JFxRwjhtNbcpA49dQ8H50cRCH1butN22oVTLQIDAQAB";
+
+var rsaPublicKeyPem = @"-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAx2fQatW9pxi0rM3nJYZ1BQB5WOVppsiR8ivcT9MChxM4V1KijYMn2xAZ/6UhRu/m
+2HKkJjVAVofmzbLlGvIVjd2lNAyMTV7srR1eQ+kwM+kXBeW5exAoB7k/KttKMkk4Gi7rMSxOxHbV
+44SQS+10LzgEhJ6sAbihrXzbplTLrW+bkp1Mkl1UB+NnHCwe3af+PZvUOQrrI4MyrBsUtvH3OBVa
+Cy16D4BjWBeUF3MZa002vPRZR2JpJpSCmkNTsykYFFJKqE3m+rQgLtDzwP+qvVqS7Ds5mNypgWEu
+TzdPSyZJGlsC0JFxRwjhtNbcpA49dQ8H50cRCH1butN22oVTLQIDAQAB
+-----END RSA PUBLIC KEY-----";
+
+// rsaPublicKey and rsaPublicKey are the same (line breaks are allowed) but PEM header is not allowed -----END RSA PUBLIC KEY-----
+
+var pubPemFormat = @"-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAx2fQatW9pxi0rM3nJYZ1BQB5WOVppsiR
+8ivcT9MChxM4V1KijYMn2xAZ/6UhRu/m2HKkJjVAVofmzbLlGvIVjd2lNAyMTV7srR1eQ+kwM+kX
+BeW5exAoB7k/KttKMkk4Gi7rMSxOxHbV44SQS+10LzgEhJ6sAbihrXzbplTLrW+bkp1Mkl1UB+Nn
+HCwe3af+PZvUOQrrI4MyrBsUtvH3OBVaCy16D4BjWBeUF3MZa002vPRZR2JpJpSCmkNTsykYFFJK
+qE3m+rQgLtDzwP+qvVqS7Ds5mNypgWEuTzdPSyZJGlsC0JFxRwjhtNbcpA49dQ8H50cRCH1butN2
+2oVTLQIDAQAB
+-----END PUBLIC KEY-----";
+
+ConvertRsa.WriteToConsole(rsaPublicKeyPkcs1, ConvertRsa.FromPublicPkcs1ToPkcs8Pem);
+ConvertRsa.WriteToConsole(rsaPublicKeyPem, ConvertRsa.FromPublicPemToPkcs8Pem);
